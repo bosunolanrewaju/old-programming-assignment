bebeaf00535707f9b55d32f6527f4b776b3b21de
Olatunbosun Olanrewaju <bosunolanrewaju@users.noreply.github.com>
2016-09-26 12:03:12 +0100

    Make D.R.Y and add a disclaimer

--- prof ID assignment/3/config/process.php
+++ prof ID assignment/3/config/process.php
@@ -1,27 +1,17 @@
 <?php 
-//Includes
-include_once "db.php";
-
-  if($_POST) {
-    if($_POST['submit']) {
-      switch ($_POST[type]) {
-        case 'login':
-          $username = $_POST['username'];
-          $password = $_POST['password'];
-          $query  = sprintf("SELECT uid FROM users WHERE username = '%s' AND password = '%s', $username, $password");
-          $result = mysqli_query($GLOBALS['DB'], $query);
-          if(mysqli_num_rows($result)) {
-            $row = mysqli_fetch_assoc($result)
-            header("dashboard.php?u=$username");
-          }
-          # code...
-          break;
-        
-        default:
-          # code...
-          break;
+  include_once "db.php";
+  // NOTE: This is not recommended to use plain text password. 
+  // Always, always store an encryted version of the password.
+  if($_POST['submit']) {
+    if ($_POST[type] == 'login' {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $query  = sprintf("SELECT uid FROM users WHERE username = '%s' AND password = '%s', $username, $password");
+      $result = mysqli_query($GLOBALS['DB'], $query);
+      if(mysqli_num_rows($result)) {
+        $row = mysqli_fetch_assoc($result)
+        header("dashboard.php?u=$username");
       }
     }
   }
-
 ?>
